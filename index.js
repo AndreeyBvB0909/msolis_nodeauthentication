99ca4a6b5b396623047f45b41faab7eee8b967e0
Michael Solis Alvarado <85146774+AndreeyBvB0909@users.noreply.github.com>
2024-08-29 09:45:24 -0600

    creando endpoints y bd-local

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { PORT } from "./config.js";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.post("/login", (req, res) => {});
+app.post("/register", (req, res) => {});
+app.post("/logout", (req, res) => {});
+app.post("/protected", (req, res) => {});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
