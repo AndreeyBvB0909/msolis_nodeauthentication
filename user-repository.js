4ed9df183937bda4da4e906a623272ced7c871d8
Michael Solis Alvarado <85146774+AndreeyBvB0909@users.noreply.github.com>
2024-08-29 09:59:26 -0600

    Creación y validaciones para crear un usuario

--- user-repository.js
+++ user-repository.js
@@ -1,13 +1,31 @@
-import DBLocal from './db-local';
-const {Schema} = new DBLocal({path: './db'});
+import DBLocal from "./db-local";
+import crypto from "crypto";
+const { Schema } = new DBLocal({ path: "./db" });
 
-const User = Schema('User',{
-    _id:{type: String, required: true},
-    username:{type: String, required: true},
-    password:{type: String, required: true}
-})
+const User = Schema("User", {
+  _id: { type: String, required: true },
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+});
 
-export class UserRepository{
-    static create({username, password}){}
-    static login({username, password}){}
+export class UserRepository {
+  static create({ username, password }) {
+    //validaciones
+    if (typeof username !== "string" || username.length < 3) {
+      throw new Error("El nombre de usuario debe tener al menos 4 caracteres");
+    }
+    if (typeof password !== "string" || password.length < 8) {
+      throw new Error("La contraseña debe tener al menos 8 caracteres");
+    }
+    //asegurarse de que el usuario no exista
+    const user = User.findOne({ username });
+    if (user) {
+      throw new Error("El usuario ya existe");
+    }
+    const _id = crypto.randomUUID();
+
+    User.create({ _id, username, password }).save();
+    return _id;
+  }
+  static login({ username, password }) {}
 }
